7b03bbc1abaa2c2f380a2fe37e70e203cb154e3e
meaza <nafkotemaye2013m@gmail.com>
2026-02-19 18:22:24 +0300

    add dark them

--- script.js
+++ script.js
@@ -7,3 +7,25 @@ function myFunction() {
         menu.style.display = "block";
     }
 }
+
+//dark mode
+const toggleBtn = document.getElementById("themeToggle");
+const body = document.body;
+
+if (localStorage.getItem("theme") === "dark") {
+    body.classList.add("dark-mode");
+    toggleBtn.textContent = "☀️ Light Mode";
+}
+
+toggleBtn.onclick = () => {
+    body.classList.toggle("dark-mode");
+
+    if (body.classList.contains("dark-mode")) {
+        localStorage.setItem("theme", "dark");
+        toggleBtn.textContent = "☀️ Light Mode";
+    } else {
+        localStorage.setItem("theme", "light");
+        toggleBtn.textContent = "🌙 Dark Mode";
+    }
+};
+
